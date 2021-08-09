4826f70b23ac377b6bfac3fc2e195b40bb25911a
Arka <arka.spectacular@gmail.com>
2021-08-09 13:57:17 +0530

    [Arka]Added:Hashtables and Binary Search Tree

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree nums = new Tree();
+            nums.Insert(50);
+            nums.Insert(17);
+            nums.Insert(23);
+            nums.Insert(12);
+            nums.Insert(19);
+            nums.Insert(54);
+            nums.Insert(9);
+            nums.Insert(14);
+            nums.Insert(67);
+            nums.Insert(76);
+            nums.Insert(72);
+        }
+    }
+}
---
+++ BinarySearchTree/Tree.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    public class Tree
+    {
+
+        public class Node
+        {
+            public int Data;
+            public Node Left;
+            public Node Right;
+            public void DisplayNode()
+            {
+                Console.Write(Data + " ");
+            }
+        }
+        public Node root;
+        public Tree()
+        {
+            root = null;
+        }
+        public void Insert(int i)
+        {
+            Node newNode = new Node();
+            newNode.Data = i;
+            if (root == null)
+                root = newNode;
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (i < current.Data)
+                    {
+                        current = current.Left;
+                        if (current == null)
+                        {
+                            parent.Left = newNode;
+                            break;
+                        }
+
+                        else
+                        {
+                            current = current.Right;
+                            if (current == null)
+                            {
+                                parent.Right = newNode;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
